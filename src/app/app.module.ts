e67fdf41bddbeb35e8d2505f32fea0fff2a087b0
rodie01 <rojo.dienst@gmail.com>
2024-02-28 17:06:40 +0100

    first push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginDropdownComponent } from './login-dropdown/login-dropdown.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginDropdownComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
