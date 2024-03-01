33c94a26cb9d51db75711bf2c747c518784d4e11
rodie01 <rojo.dienst@gmail.com>
2024-03-01 16:38:29 +0100

    Html Projekt erstellung erster Entwurf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { UserManagmentPageComponent } from './user-managment-page/user-managment-page.component';
 import { UserProfilComponent } from './user-profil/user-profil.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
   {path: 'userManagment', component: UserManagmentPageComponent},
-  {path: 'profil', component: UserProfilComponent}
+  {path: 'profil', component: UserProfilComponent},
+  {path: 'createProject', component: CreateProjectComponent}
   //{ path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
 
